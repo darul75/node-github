c2198a4fac9d4f780f4a57dc18743e81604818d2
darul75 <darul75@gmail.com>
2014-12-24 15:33:53 +0100

    zoubida

--- test/oauth.js
+++ test/oauth.js
@@ -8,12 +8,22 @@ var OAuth2 = require("oauth").OAuth2;
 
 var github = new Client({
   version: "1",
-  pathPrefix:"v1"
+  pathPrefix:"v1",
+  proxy: {
+    host: '10.115.100.103',
+    port: 8080
+  }
 });
 
 var clientId = "7782fkexleri0s";
 var secret = "29X0lbFr6GvoJM8S";
 var oauth = new OAuth2(clientId, secret, "https://www.linkedin.com/", "uas/oauth2/authorization", "uas/oauth2/accessToken");
+oauth.setTunnel({
+  proxy: {
+    host: '10.115.100.103',
+    port: 8080
+  }
+});
 
 // for demo purposes use one global access token
 // in production this has to be stored in a user session
@@ -83,14 +93,43 @@ app.get('/people', function (req, res) {
     });
   };
 
-  var wrapper = function(callback) {
-    github.people.getCurrent({"url-field-selector": ':(id,first-name,last-name,industry,connections,group-memberships,educations,date-of-birth,positions)'}, function(err, result) {
-      result['_NAME'] = 'github.people.getCurrent';
+  var peopleCurrent = function(callback) {
+    github.people.get({"url-field-selector": ':(id,first-name,last-name,industry,connections,group-memberships,educations,date-of-birth,positions)'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.people.get';
       callback(null, result);
     });    
   };
 
-  var wrapper2 = function(callback) {
+  var peopleCurrentConnections = function(callback) {
+    github.people.getCurrentConnections({"url-field-selector": ':(headline,first-name,last-name)'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.people.get-current-connections';
+      callback(null, result);
+    });    
+  };
+
+  var peopleMember = function(callback) {
+    github.people.getMember({"id": '2JVl6yrion'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.people.getMember';
+      callback(null, result);
+    });    
+  };
+
+  var peopleMemberConnections = function(callback) {
+    github.people.getMemberConnections({"id": '2JVl6yrion'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.people.getMember';
+      callback(null, result);
+    });    
+  };    
+
+  var groupsMembership = function(callback) {
     github.groups.getMemberships({}, function(err, result) {
       if (err)
         result = {"error" : err};
@@ -99,16 +138,55 @@ app.get('/people', function (req, res) {
     });
   };
 
-  var wrapper3 = function(callback) {
-    github.company.getCurrent({}, function(err, result) {
+  // use github API            
+  q.defer(ignoreError, peopleCurrent);
+  q.defer(ignoreError, peopleCurrentConnections);
+  q.defer(ignoreError, peopleMember);
+  q.defer(ignoreError, peopleMemberConnections);
+  q.defer(ignoreError, groupsMembership);
+  
+  // q.defer(github.groups.getMemberships, {});  
+  q.awaitAll(function(error, results) {     
+    if (error)
+      res.json(error);
+    res.json(results);
+  });
+
+  
+});
+
+app.get('/company', function (req, res) {
+
+  var q = queue(1);
+
+  var ignoreError = function (task, callback) {
+    task(function(error, result) {
+      return callback(null, result); // ignore error
+    });
+  };
+  
+  var companyCurrent = function(callback) {
+    github.company.all({}, function(err, result) {
       if (err)
         result = {"error" : err};
-      result['_NAME'] = 'github.company.getCurrent';
+      result['_NAME'] = 'github.company.all';
       callback(null, result);
     });
   };
 
-  var wrapper4 = function(callback) {
+  var companyOneAndFields = function(callback) {
+    github.company.one({
+      'company-id': 1337,
+      'url-field-selector': ':(id,name)'
+    }, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.companyOneAndFields';
+      callback(null, result);
+    });
+  };
+
+  var companyOne = function(callback) {
     github.company.one({"company-id": 162479}, function(err, result) {
       if (err)
         result = {"error" : err};
@@ -116,16 +194,96 @@ app.get('/people', function (req, res) {
       callback(null, result);
     });
   };
+
+  var companyUniversalName = function(callback) {
+    github.company.oneByUniversalName({"universal-name": 'linkedin'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.oneByUniversalName';
+      callback(null, result);
+    });
+  };
+
+  var companyEmailDomain = function(callback) {
+    github.company.oneByEmailDomain({"email-domain": 'linkedin.com'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.companyEmailDomain';
+      callback(null, result);
+    });
+  };
+
+  var companyCurrentIsAdmin = function(callback) {
+    github.company.all({'is-company-admin': true}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.all';
+      callback(null, result);
+    });
+  };
+
+  var companyMixed = function(callback) {
+    github.company.all({'url-field-selector': '::(162479,universal-name=linkedin)'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.all';
+      callback(null, result);
+    });
+  };
+
+  var companyOneUpdate = function(callback) {
+    github.company.oneUpdate({"company-id": 162479}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.oneUpdate';
+      callback(null, result);
+    });
+  };
+
+  var companyOneUpdatePagination = function(callback) {
+    github.company.oneUpdate({"company-id": 162479, "start":0, "count":5}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.companyOneUpdatePagination';
+      callback(null, result);
+    });
+  };
+
+  var companyOneUpdateEventType = function(callback) {
+    github.company.oneUpdate({"company-id": 162479, 'event-type': 'status-update'}, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.oneUpdateEventType';
+      callback(null, result);
+    });
+  };
+
+  var companyOneUpdateComments = function(callback) {
+    github.company.oneUpdateComments({"company-id": 162479, 
+      'company-update-key': 'UPDATE-c162479-5953413562058702849',
+      'event-type': 'CMPY'
+    }, function(err, result) {
+      if (err)
+        result = {"error" : err};
+      result['_NAME'] = 'github.company.companyOneUpdateComments';
+      callback(null, result);
+    });
+  };
   
-  // use github API            
-  q.defer(ignoreError, wrapper);
-  q.defer(ignoreError, wrapper2);
-  q.defer(ignoreError, wrapper3);
-  q.defer(ignoreError, wrapper4);
-  // q.defer(github.people.getCurrent, {"url-field-selector": ':(id,first-name,last-name,industry,connections,group-memberships,educations,date-of-birth,positions)'});
-  // q.defer(github.people.getCurrent, {"url-field-selector": ':(id,first-name,last-name,industry,connections,group-memberships,educations,date-of-birth,positions)'});
-  // q.defer(github.people.getCurrentConnections, {});
-  // q.defer(github.groups.getMemberships, {});  
+  // use github API
+  q.defer(ignoreError, companyCurrent);
+  q.defer(ignoreError, companyOne);
+  q.defer(ignoreError, companyOneAndFields);
+  q.defer(ignoreError, companyOneUpdate);
+  q.defer(ignoreError, companyOneUpdateEventType);
+  q.defer(ignoreError, companyOneUpdatePagination);  
+  q.defer(ignoreError, companyOneUpdateComments);  
+  q.defer(ignoreError, companyUniversalName);
+  q.defer(ignoreError, companyEmailDomain);
+  q.defer(ignoreError, companyCurrentIsAdmin);
+  q.defer(ignoreError, companyMixed);
+
+  
   // q.defer(github.groups.getMemberships, {});  
   q.awaitAll(function(error, results) {     
     if (error)
